7ceda5dc985cc0efd9bda32d9e666a4e9f0050b4
emudassar <contacmudassar@gmail.com>
2025-08-10 09:51:49 +0500

    route and querry parameter practice

--- express/app.js
+++ express/app.js
@@ -4,14 +4,19 @@ import path from "path";
 
 const app = express();
 
-const staticPath = path.join(import.meta.dirname, "public")
-app.use(express.static(staticPath))
+// const staticPath = path.join(import.meta.dirname, "public");
 
-// app.get("/", (req, res) => {
-//   const homePagePath = path.join(import.meta.dirname, "public", "index.html");
-//   res.sendFile(homePagePath);
-// });
+// app.use(express.static(staticPath));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page!");
+});
+
+app.get("/profile/:username", (req, res) => {
+  console.log(req.params);
+  res.send(`hii ${req.params.username}`);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`); 
 });
