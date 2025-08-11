123fe4545eadac9bf940c68d3ff0505bf9adce4a
emudassar <contacmudassar@gmail.com>
2025-08-11 09:43:53 +0500

    handle form submission

--- express/app.js
+++ express/app.js
@@ -1,22 +1,38 @@
 import express from "express";
 import { PORT } from "./env.js";
-import path from "path";
+import path, { join } from "path";
 
 const app = express();
 
-// const staticPath = path.join(import.meta.dirname, "public");
+const staticPath = path.join(import.meta.dirname, "public");
 
-// app.use(express.static(staticPath));
+app.use(express.static(staticPath));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the home page!");
+app.use(express.urlencoded())
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the home page!");
+// });
+
+// app.get("/profile/:username", (req, res) => {
+//   console.log(req.params);
+//   res.send(`hii ${req.params.username}`);
+// });
+
+// app.get("/contact", (req, res) => {
+//   console.log(req.query);
+//   res.redirect("/");
+// });
+
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
 });
 
-app.get("/profile/:username", (req, res) => {
-  console.log(req.params);
-  res.send(`hii ${req.params.username}`);
-});
+app.use((req, res) => {
+return res.status(404).sendFile(path.join(import.meta.dirname, "views", "404.html"))
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`); 
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
