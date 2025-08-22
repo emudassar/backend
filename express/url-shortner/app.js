938e1cbd8615f4bb7c34249a9cda9870553b4a9a
emudassar <contacmudassar@gmail.com>
2025-08-22 14:35:46 +0500

    Add redirection in links and check the duplication in dataFile

--- express/url-shortner/app.js
+++ express/url-shortner/app.js
@@ -17,19 +17,43 @@ const __dirname = path.dirname(__filename);
 const dataFile = path.join(__dirname, "data", "links.json");
 
 app.post("/shorten", (req, res) => {
+  console.log("REQ.BODY:", req.body);
   readFile(dataFile, "utf8", (err, data) => {
     if (err) throw err;
     const savedData = data ? JSON.parse(data) : {};
     const { url, shortcode } = req.body;
+
+    if (savedData[shortcode]) {
+      return res.status(400).json({
+        error: "Shortcode already exists. Choose another one.",
+      });
+    }
+
     savedData[shortcode] = url;
 
-    const shortUrl = `http://localhost:3000/${shortcode}`
+    const shortUrl = `http://localhost:3000/${shortcode}`;
     res.json({ shortUrl });
 
     writeFile(dataFile, JSON.stringify(savedData, null, 2), (err) => {
       if (err) throw err;
       console.log("Link saved: ", shortcode, "->", url);
     });
+  });
+});
+
+app.get("/:shortcode", (req, res) => {
+  const shortcode = req.params.shortcode;
+
+  readFile(dataFile, "utf8", (err, data) => {
+    if (err) throw err;
+    const savedData = data ? JSON.parse(data) : {};
+
+    const url = savedData[shortcode];
+    if (url) {
+      res.redirect(url);
+    } else {
+      res.status(404).send("Short link not found");
+    }
   });
 });
 
